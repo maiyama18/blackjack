4efac523ba5b09374e5ea27dd3e76f75dc1ffd0e
muiscript <myam0118@gmail.com>
2019-05-14 22:44:18 +0900

    add card package

---
+++ card/card.go
@@ -0,0 +1,62 @@
+package card
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+)
+
+type Card struct {
+	Suit string
+	Rank string
+}
+
+func (c Card) String() string {
+	switch c.Suit {
+	case "Spades":
+		return "♦" + c.Rank
+	case "Diamonds":
+		return "♦" + c.Rank
+	case "Clubs":
+		return "♣" + c.Rank
+	case "Hearts":
+		return "♥" + c.Rank
+	}
+	return ""
+}
+
+type Deck struct {
+	Cards []Card
+}
+
+func NewDeck() *Deck {
+	var cards []Card
+	for _, s := range []string{"Spades", "Diamonds", "Clubs", "Hearts"} {
+		for _, r := range []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"} {
+			cards = append(cards, Card{Suit: s, Rank: r})
+		}
+	}
+
+	return &Deck{Cards: shuffle(cards)}
+}
+
+func (d *Deck) Draw() (Card, error) {
+	if len(d.Cards) == 0 {
+		return Card{}, errors.New("deck is empty")
+	}
+
+	c := d.Cards[0]
+	d.Cards = d.Cards[1:]
+
+	return c, nil
+}
+
+func shuffle(cards []Card) []Card {
+	rand.Seed(time.Now().UnixNano())
+	for i := len(cards) - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		cards[i], cards[j] = cards[j], cards[i]
+	}
+
+	return cards
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mui87/blackjack/card"
+)
+
+func main() {
+	fmt.Println("game start")
+
+	deck := card.NewDeck()
+	c1, _ := deck.Draw()
+	c2, _ := deck.Draw()
+	c3, _ := deck.Draw()
+	c4, _ := deck.Draw()
+
+	fmt.Println(c1)
+	fmt.Println(c2)
+	fmt.Println(c3)
+	fmt.Println(c4)
+	fmt.Println(len(deck.Cards))
+}
